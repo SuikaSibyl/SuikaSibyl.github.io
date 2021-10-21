ea70ab6f941ef9d615b06a0716c3c6015940e56c
SuikaSibyl <60009633+SuikaSibyl@users.noreply.github.com>
2021-10-22 00:00:38 +0800

    test

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -19,6 +19,7 @@
                     <a v-if="atwork==1" :href="'../../#' + m.id" @click="closeMenuDrawer">{{m.name}}</a>
                 </a-menu-item>
             </a-menu>
+             <a href="about">hello</a>
 
         </a-layout-content>
         <!-- 菜单页脚 -->
